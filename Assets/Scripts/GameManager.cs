891b8461818f7fb5c80c22bc38f70e115d4d97c2
Marc-Olivier Belisle <islandx424@gmail.com>
2020-01-31 23:45:37 -0500

    Start implementing GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameType
+{
+    Work,
+    Study,
+    Sleep,
+    Count
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameType[] scenes;
+
+    GameType game;
+    List<int>[] gameLists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int gametypeCount = (int)GameType.Count;
+        gameLists = new List<int>[gametypeCount];
+        for (int i = 0; i < gametypeCount; i++)
+            gameLists[i] = new List<int>();
+
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            int type = (int)scenes[i];
+            gameLists[type].Add(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
