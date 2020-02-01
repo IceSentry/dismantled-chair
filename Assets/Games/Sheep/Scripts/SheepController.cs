223628adfaeea93a35274a18ca51db1b732144af
Marc-Olivier Belisle <islandx424@gmail.com>
2020-01-31 22:57:15 -0500

    Add simple sheep game

---
+++ Assets/Games/Sheep/Scripts/SheepController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+
+        rigid.MovePosition(Vector3.Lerp(transform.position, worldPos, speed * Time.deltaTime));
+    }
+}
